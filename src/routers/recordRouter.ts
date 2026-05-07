f93cad5b95aef7fc85f88b4d63288c2f33a74fa7
Alvaro Martin <alu0101646299@ull.edu.es>
2026-05-07 17:27:55 +0000

    recordRouter GET

--- src/routers/recordRouter.ts
+++ src/routers/recordRouter.ts
@@ -69,3 +69,54 @@ recordsRouter.post("/records", async (req, res) => {
     return res.status(500).send(error);
   }
 });
+
+recordsRouter.get("/records", async (req, res) => {
+  try {
+    let filtro = {};
+    if (req.query.dni) {
+      filtro = { dni: req.query.dni.toString() };
+      const paciente = await Patient.findOne(filtro);
+      if (!paciente) { return res.status(404).send({ error: "Paciente no encontrado" }) }
+
+      const registros = await Record.find({ paciente: paciente._id }).sort({ fechaInicio: 1 });
+      res.send(registros);
+    } else if (req.query.fechaInicio && req.query.fechaFin) {
+      if (req.query.tipo) { 
+        filtro = {
+          fechaInicio: {
+            $gte: new Date(req.query.fechaInicio.toString()),
+            $lte: new Date(req.query.fechaFin.toString())
+          },
+          tipo: req.query.tipo
+        };
+      } else {
+        filtro = {
+          fechaInicio: {
+            $gte: new Date(req.query.fechaInicio.toString()),
+            $lte: new Date(req.query.fechaFin.toString())
+          }
+        };
+      }
+
+      const registros = await Record.find(filtro);
+      res.send(registros);
+    } else {
+      res.status(400).send("Falta indicar número de identificación del paciente o un rango de fechas");
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+recordsRouter.get("/records/:id", async (req, res) => {
+  try {
+    const registro = await Record.findById(req.params.id);
+    if (registro) {
+      res.send(registro);
+    } else {
+      res.status(404).send();
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
