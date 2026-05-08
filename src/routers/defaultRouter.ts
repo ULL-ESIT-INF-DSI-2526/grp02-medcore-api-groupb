2b68a74392c55b0b7746b74fcc79ca06d52e1d28
Alvaro-MPD <alu0101646299@ull.edu.es>
2026-05-08 20:43:26 +0100

    Cambios en router

--- src/routers/defaultRouter.ts
+++ src/routers/defaultRouter.ts
@@ -2,7 +2,7 @@ import express from 'express';
 
 export const defaultRouter = express.Router();
 
-defaultRouter.all('/(.*)', (req, res) => {
+defaultRouter.all('/{*splat}', (req, res) => {
   res.status(404).send({ 
     error: `La ruta ${req.originalUrl} no existe` 
   });
